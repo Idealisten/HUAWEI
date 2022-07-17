004a6b05e1c31e4117833aaa557bdeea8d55175d
CAOYUE <prometheus.mr.cy@gmail.com>
2022-07-17 21:49:50 +0800

    动态规划 递归

---
+++ 91.走方格的方案数.py
@@ -0,0 +1,16 @@
+# 动态规划 递归
+
+n, m = map(int, input().split())
+
+
+def steps_of_square(a, b):
+    if a == 0 and b == 0:
+        return 0
+    elif a == 0 or b == 0:
+        return 1
+    else:
+        return steps_of_square(a, b-1) + steps_of_square(a-1, b)
+
+
+print(steps_of_square(n, m))
+

6c46f2c56901430fdec3e12f0db475d28407437e
CAOYUE <prometheus.mr.cy@gmail.com>
2022-08-16 22:20:32 +0800

    中心扩散法

---
+++ 32.密码截取-中心扩散法.py
@@ -0,0 +1,20 @@
+password = input()
+longest = 0
+
+
+def extend_from_center(passwd, left, right):
+    global longest
+    while left >= 0 and right < len(passwd) and passwd[left] == passwd[right]:
+        left -= 1
+        right += 1
+    if right - left + 1 > longest:
+        longest = left - right + 1
+
+
+for i in range(0, len(password)):
+    extend_from_center(password, i, i)
+    extend_from_center(password, i, i+1)
+
+print(longest)
+
+

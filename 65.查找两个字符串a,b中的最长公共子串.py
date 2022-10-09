276717cdffef0b3a6727eff822ace19144c7e278
CAOYUE <prometheus.mr.cy@gmail.com>
2022-10-09 23:58:29 +0800

    暴力法，通过全部测试用例

--- 65.查找两个字符串a,b中的最长公共子串.py
+++ 65.查找两个字符串a,b中的最长公共子串.py
@@ -1,30 +1,22 @@
-str1 = "nvlrzqcjltmrejybjeshffenvkeqtbsnlocoyaokdpuxutrsmcmoearsgttgyyucgzgcnurfbubgvbwpyslaeykqhaaveqxijc"
-str2 = "wkigrnngxehuiwxrextitnmjykimyhcbxildpnmrfgcnevjyvwzwuzrwvlomnlogbptornsybimbtnyhlmfecscmojrxekqmj"
+"""
+暴力解法，注意要把较短的字符串放在外层循环，否则在有相同长度子串时只会记录较长字符串中第一个出现的
+"""
+str1 = input()
+str2 = input()
+if len(str1) > len(str2):
+    t = str1
+    str1 = str2
+    str2 = t
 ans = 0
-begin = []
-length = []
-print(len(str1))
-print(len(str2))
+begin = 0
+length = 0
 for i in range(len(str1)):
     for j in range(len(str2)):
         k = 0
-        while i + k < len(str1) and j + k < len(str2):
-            if str1[i + k] == str2[j + k]:
-                k += 1
+        while i + k < len(str1) and j + k < len(str2) and str1[i + k] == str2[j + k]:
+            k += 1
         if k > ans:
             ans = k
-            if len(str1) < len(str2):
-                begin.append(i)
-                length.append(k)
-                print("sub_str1:{}".format(str1[i:i+k]))
-            else:
-                begin.append(j)
-                length.append(k)
-                print("sub_str2:{}".format(str2[j:j+k]))
-            print("ans:{}".format(ans))
-
-
-# if len(str1) < len(str2):
-#     print(str1[begin:begin+length])
-# else:
-#     print(str2[begin:begin+length])
+            begin = i
+            length = k
+print(str1[begin:begin+length])
